64365a84a436eca6f094ef1ec77ec17056920da1
MonroyNavarro, Rene <Rene.Monroy@nike.com>
2017-06-12 12:42:55 -0500

    Move cli execution directly into bin

--- bin/preact-init.js
+++ bin/preact-init.js
@@ -1,4 +1,42 @@
 #!/usr/bin/env node
 'use strict';
 
-require('../lib/cli');
+const minimist = require('minimist');
+
+const init = require('../lib/init');
+const { showHelp, showVersion } = require('../lib/utils');
+
+const command = minimist(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    v: 'version',
+  },
+});
+
+const config = Object.assign({
+  name: 'my-project',
+  repository: 'https://github.com/churris/preact-simple-template.git',
+}, command);
+delete config['_'];
+
+switch (command['_'].length) {
+  case 0:
+    break;
+  case 1:
+    config.name = command['_'][0];
+    break;
+  case 2:
+    config.name = command['_'][0];
+    config.repository = command['_'][1];
+    break;
+  default:
+    showHelp();
+}
+
+if (config.help) {
+  showHelp();
+} else if (config.version) {
+  showVersion();
+} else {
+  init(config);
+}
